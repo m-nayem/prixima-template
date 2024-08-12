fec8f4c55be22ea22a54fd02513d4cdea17bb4fa
mohammad-nayem <nishanahmed13913@gmail.com>
2024-08-13 01:38:21 +0600

    prixima template

---
+++ JS/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+     // HERO SLIDER 
+     $('#hero-area').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        items:1,
+        dots:false,
+        navText:["Prev","Next"],
+        smartSpeed:1000,
+        responsive:{
+            0:{
+                 
+            },
+            600:{
+                
+            },
+            1000:{
+                 
+            }
+        }
+    })
+})
